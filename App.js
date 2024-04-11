422304f736a54f2ab3440c2189af4de8efc18ed0
Rixuan Zheng <zheng.rix@northeastern.edu>
2024-04-11 01:09:43 -0400

    a6

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
+import "dotenv/config";
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
+import mongoose from "mongoose";
 import Hello from './Hello.js';
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./kanbas/courses/routes.js";
 import ModuleRoutes from './kanbas/modules/routes.js';
+import UserRoutes from "./users/routes.js";
 const app = express();
-app.use(cors());
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
+const CONNECTION_URL = "mongodb+srv://rixuanzheng:92TY%40lim%40@cs4550.o4kllhy.mongodb.net/kanbas?retryWrites=true&w=majority";
+mongoose.connect(CONNECTION_URL);
 Hello(app);
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
