81d839858a7dba73395cb45287f6a6869b8dc474
Rixuan Zheng <zheng.rix@northeastern.edu>
2024-04-14 19:31:40 -0400

    a6 modules

--- kanbas/courses/dao.js
+++ kanbas/courses/dao.js
@@ -6,4 +6,4 @@ export const createCourse = (course) => {
 export const findAllCourses = () => model.find();
 export const updateCourse = (courseId, course) => model.updateOne({ _id: courseId }, { $set: course });
 export const deleteCourse = (courseId) => model.deleteOne({ _id: courseId });
-export const findCourseById = (courseId) => model.findById(courseId);
+export const findCourseById = (courseId) => model.findOne({ id: courseId });
